34c1de93958a3e7085d46360a26bfc0ab62c6b8c
andantender <chloelimyi+git@gmail.com>
2014-02-19 19:35:16 -0500

    ChloeAttempt2 can have changes in pages dynamically!

--- ChloeAttempt2/js/main.js
+++ ChloeAttempt2/js/main.js
@@ -13,6 +13,26 @@ var app = {
     alert('after placeholder');
 
 
+
+
+    //retrieve json from external json file
+    // $.getJSON ('./data/parks.json', function (json) {
+      
+    //   alert('gotthisjson');
+    //   // alert(json.parks[0].name);
+
+
+    //   var src = $('#centerContainer-template').html(),
+      
+    //   template = Handlebars.compile(src),
+    //   data = template(json),
+    //   html = $('#dyn_content').html(data);
+    //   $('ul').listview('refresh');
+
+      
+
+    // })
+
     $.getJSON ('./data/parks.json', function (json) {
       
       // alert('gotthisjson');
@@ -21,31 +41,15 @@ var app = {
       template = Handlebars.compile(src),
       data = template(json),
       html = $('body').html(data);
-      
-    })
-
-    //retrieve json from external json file
-    $.getJSON ('./data/parks.json', function (json) {
-      
-      alert('gotthisjson');
-      // alert(json.parks[0].name);
-
-
-      var src = $('#centerContainer-template').html(),
-      
-      template = Handlebars.compile(src),
-      data = template(json),
-      html = $('#dyn_content').html(data);
       $('ul').listview('refresh');
-
+      
       $.each(json.parks, function(i) {
           alert(json.parks[i].name);
           alert('good');
           var pagename = json.parks[i].name.toLowerCase().replace(/\s/g, '');
           $('#'+pagename).trigger('pagecreate')
       }); 
-
-    })
+    })    
 
 
   },   
