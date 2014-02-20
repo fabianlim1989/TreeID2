d3dac83f24b6da7846c011c01cc987362f92b465
Fabian Lim <fabianlim1989@gmail.com>
2014-02-19 23:26:24 -0500

    chloe attempt 2

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+//Handlebar helper classes
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase().replace(/\s/g, '');
+});
+
+Handlebars.registerHelper('retrieve', function(num) {
+  $.getJSON ('./data/parks.json', function (json) {
+    	return json.parks[num];
+    });
+});
+
+function goToNext(){
+   $.getJSON ('./data/parks.json', function (json) {
+     alert(json.parks[0].name);
+   });
+}
+
+function renderschenley()
+{
+	alert("Hello World!");
+	$.getJSON ('./data/parks.json', function (json) {
+      
+      alert('gotthisjsonforschenley');
+      var park = json.parks[0];
+      alert(park.name);
+      var src = $('#allparks-template').html(),
+      template = Handlebars.compile(src),
+      data = template(park),
+      html = $('#otherpages').html(data);
+      
+
+      // $.each (json.parks, function (i){
+      //   alert('eaching' + i + json.parks[i].name);
+        
+      //   var src = $('#allparks-template').html(),
+      //   template = Handlebars.compile(src),
+      //   data = template(json),
+      //   html = $('body').html(data);
+      //   alert('trying');
+
+      // });
+    })
+}
+
--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
-var app = {
+var app = {  
+  
+  showAlert: function (message, title) {  
+    if (navigator.notification) {  
+     navigator.notification.alert(message, null, title, 'OK');  
+   } else {  
+     alert(title ? (title + ": " + message) : message);  
+   }  
+  },
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+  renderSplashView: function() {  
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    //alert('after placeholder');
 
+    $.getJSON ('./data/parks.json', function (json) {
+      
+      // alert('gotthisjson');
+
+      //takes allparks-template as the source template
+      var src = $('#allparks-template').html();
+      
+      //compiles it to make it a template
+      template = Handlebars.compile(src);
+
+      //puts the json data (parks data) into the template
+      data = template(json);
+
+      //'unloads' all this stuff into the <body> tag
+      html = $('body').html(data);
+
+      //ensures that the elements li in the unordered list get jquery formatting
+      $('ul').listview('refresh');
+      
+      $.each(json.parks, function(i) {
+          // alert(json.parks[i].name);
+          // alert('good');
+          
+          //sets the pagename correctly (trimming and removing spaces)
+          var pagename = json.parks[i].name.toLowerCase().replace(/\s/g, '');
+          
+          //creates a new page for each park
+          //$('#'+pagename).trigger('pagecreate')
+          //doesn't seem necessary after all
+      }); 
+    })    
+
+
+  },   
+
+  initialize: function() {  
+    //alert('BeforeInitialize');
+    var self = this;  
+    this.renderSplashView();  
+
+  }  
 };
 
-app.initialize();
+app.initialize();  
