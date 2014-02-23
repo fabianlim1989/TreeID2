5666e1814651dbc4ccb7728af9af5d389a65fdb2
Fabian Lim <fabianlim1989@gmail.com>
2014-02-22 21:31:27 -0500

    GPS working?

--- js/gps.js
+++ js/gps.js
@@ -5,16 +5,16 @@ var watchID = null;
 function watchGPS() {
   // Update every 3 seconds
   //
-  var options = { frequency: 3000 };
+  var options = { frequency: 5000, enableHighAccuracy: true };
   //navigator.geolocation.getCurrentPosition(onGPSSuccess, onGPSError);
   watchID = navigator.geolocation.watchPosition(onGPSSuccess, onGPSError, options);
-  console.log("watchID: "+watchID);
+  console.log("watch watchID: "+watchID);
 }
 
 // onGPSSuccess Geolocation
 //
 function onGPSSuccess(position) {
-  var element = document.getElementById('geolocation');
+  //var element = document.getElementById('geolocation');
   var info =    'Latitude: '           + position.coords.latitude              + '<br />' +
                 'Longitude: '          + position.coords.longitude             + '<br />' +
                 'Altitude: '           + position.coords.altitude              + '<br />' +
@@ -23,13 +23,19 @@ function onGPSSuccess(position) {
                 'Heading: '            + position.coords.heading               + '<br />' +
                 'Speed: '              + position.coords.speed                 + '<br />' +
                 'Timestamp: '          + position.timestamp                    + '<br />';
-  element.innerHTML = info;
+  //element.innerHTML = info;
   console.log(info);
 }
 
 // onGPSError Callback receives a PositionError object
 //
 function onGPSError(error) {
-  alert('code: '    + error.code    + '\n' +
-        'message: ' + error.message + '\n');
+  console.log('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+  if(watchID != null){
+    console.log("clear watchID: "+watchID);
+    navigator.geolocation.clearWatch(watchID);
+    watchID = null;
+  }
+  alert("Could not retrieve GPS information. Please ensure that your device's GPS is working and try again.");
 }
